57b2fd766694aec5447c43dec78b246e82577103
Swapnil Sharma <swap.sha96@gmail.com>
2017-10-02 13:04:22 +0530

    Created PIP package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(name='Italian Vip Says',
+	version='1.0',
+	url='https://github.com/lotrekagency/italian-vip-says',
+	license='MIT',
+	author='Lotrek',
+	author_email='',
+	description='',
+	long_description=open('README.md').read())
